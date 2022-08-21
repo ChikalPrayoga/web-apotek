a6209acc71d209de831dda068aa5b6d81d00dc32
T. Yulis <timuryulis14@gmail.com>
2022-08-21 16:35:59 +0700

    push buat tugas

---
+++ README.md
@@ -0,0 +1,9 @@
+# Apotek Sederhana
+
+Aplikasi ini untuk melengkapi tugas mata kuliah Pemrograman Web Lanjut semester VI Prodi Informatika UNINDRA PGRI, yang dibangun dengan framewaork CodeIgniter 3
+
+
+# Login
+
+Username = admin
+Password = 123
---
+++ application/views/partial/footer.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Timur Yulis Santosa | 201943500336 </span>
+        </div>
+    </div>
+</footer>
